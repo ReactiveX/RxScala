5dfc968ed518de41721bcd3d2bc82e1b4975356a
Joachim Hofer <jmhofer.github@johoop.de>
2014-09-26 23:59:50 +0200

    Cross-publishing via Travis Scala versions.

--- publishViaTravis.sh
+++ publishViaTravis.sh
@@ -6,7 +6,7 @@ echo -e 'Bintray Upload Script => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Bintray Upload => Starting upload ...\n'
 
-  sbt storeBintrayCredentials clean +publish
+  sbt ++$TRAVIS_SCALA_VERSION storeBintrayCredentials clean publish
   RETVAL=$?
 
   if [ $RETVAL -eq 0 ]; then
