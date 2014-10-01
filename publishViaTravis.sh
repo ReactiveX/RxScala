a9c96b7a87ac2040dd6896aa2ff2aff483ee094b
Ben Christensen <benjchristensen@gmail.com>
2014-10-01 12:20:35 -0700

    Update publishViaTravis.sh

--- publishViaTravis.sh
+++ publishViaTravis.sh
@@ -4,6 +4,12 @@
 echo -e 'Bintray Upload Script => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
+
+  echo -e 'Bintray Upload => Decrypting keys ...\n'
+
+  openssl aes-256-cbc -K $encrypted_506de5234d8d_key -iv $encrypted_506de5234d8d_iv -in key.private.F984A085.asc.enc -out key.private.F984A085.asc -d
+  openssl aes-256-cbc -K $encrypted_506de5234d8d_key -iv $encrypted_506de5234d8d_iv -in key.public.F984A085.asc.enc -out key.public.F984A085.asc -d
+
   echo -e 'Bintray Upload => Starting upload ...\n'
 
   sbt storeBintrayCredentials
