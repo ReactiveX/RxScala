1775a86e066296657ccf0995229d2f5b1ef87692
zsxwing <zsxwing@gmail.com>
2015-03-23 12:09:34 +0800

    Sync to Sonatype if publishing successfully

--- publishViaTravis.sh
+++ publishViaTravis.sh
@@ -18,6 +18,19 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
     echo 'Upload failed.'
     exit 1
   fi
+
+  # snyc to Sonatype
+  export SONA_USER=$sonatypeUsername
+  export SONA_PASS=$sonatypePassword
+  sbt syncMavenCentral
+  RETVAL=$?
+  if [ $RETVAL -eq 0 ]; then
+    echo 'Completed sync to Sonatype!'
+  else
+    echo 'Sync to Sonatype failed.'
+    exit 2
+  fi
+
 else
   echo 'Bintray Upload => Not a tagged build so will not upload'
 fi
