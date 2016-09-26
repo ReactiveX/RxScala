50e15cb2a4101949750aa949f94304ee3f3fce89
Richard van Heest <richard.van.heest@dans.knaw.nl>
2016-09-26 14:53:32 +0200

    with tests...

---
+++ src/test/scala/rx/lang/scala/ConversionsTests.scala
@@ -0,0 +1,55 @@
+package rx.lang.scala
+
+import org.junit.Test
+import org.scalatest.junit.JUnitSuite
+import rx.lang.scala.observers.TestSubscriber
+
+import scala.util.{Failure, Success}
+
+class ConversionsTests extends JUnitSuite {
+
+  @Test
+  def testTrySuccessConversion() = {
+    val success = Success("abc")
+    val observer = TestSubscriber[String]()
+    success.toObservable.subscribe(observer)
+
+    observer.assertValue("abc")
+    observer.assertNoErrors()
+    observer.assertCompleted()
+  }
+
+  @Test
+  def testTryFailureConversion() = {
+    val error = new IllegalArgumentException("test error")
+    val failure = Failure[String](error)
+    val observer = TestSubscriber[String]()
+    failure.toObservable.subscribe(observer)
+
+    observer.assertNoValues()
+    observer.assertError(error)
+    observer.assertNotCompleted()
+  }
+
+  @Test
+  def testOptionSomeConversion() = {
+    val some = Some("abc")
+    val observer = TestSubscriber[String]()
+    some.toObservable.subscribe(observer)
+
+    observer.assertValue("abc")
+    observer.assertNoErrors()
+    observer.assertCompleted()
+  }
+
+  @Test
+  def testOptionNoneConversion() = {
+    val some = None
+    val observer = TestSubscriber[String]()
+    some.toObservable.subscribe(observer)
+
+    observer.assertNoValues()
+    observer.assertNoErrors()
+    observer.assertCompleted()
+  }
+}
